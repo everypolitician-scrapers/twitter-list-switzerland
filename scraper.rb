49eeac1c673146a7b0ffb0958cc243e58cb398ac
Oliver Denman <oliver@therewillbeanimals.com>
2016-08-04 13:13:32 +0100

    fix error

--- scraper.rb
+++ scraper.rb
@@ -10,7 +10,7 @@ require 'twitter_list'
 require 'pry'
 
 abort "Need to set ENV['MORPH_TWITTER_TOKENS']" unless ENV.key? 'MORPH_TWITTER_TOKENS'
-twitter_list = TwitterList::Scraper.new(twitter_tokens: 'ENV['MORPH_TWITTER_TOKENS']')
+twitter_list = TwitterList::Scraper.new(twitter_tokens: ENV['MORPH_TWITTER_TOKENS'])
 
 twitter_list_arr = (
 twitter_list.people('ParlamentTweets', 'glp-parlamentarier') +
